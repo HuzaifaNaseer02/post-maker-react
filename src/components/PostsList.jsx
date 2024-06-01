607a260dcee3ea6b63ba2230e213e6db948a7c7a
Huzaifa Naseer Khan <huzaifanaseerkhan02@gmail.com>
2024-06-01 13:24:09 +0500

    Used loader() from react router dom for fetching data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,21 +1,9 @@
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const PostsList = () => {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const data = await response.json();
-      setPosts(data.posts);
-      setIsFetching(false);
-    }
-    fetchPosts();
-  }, []);
+  const posts = useLoaderData();
 
   const addPostHandler = (postData) => {
     fetch("http://localhost:8080/posts", {
@@ -30,21 +18,16 @@ const PostsList = () => {
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts!</h2>
-        </div>
-      )}
-      {isFetching && (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <p>Loading posts...</p>
         </div>
       )}
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Posts from "./routes/Posts";
+import Posts, { loader as postsLoader } from "./routes/Posts";
 import NewPost from "./routes/NewPost";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Posts />,
+        loader: postsLoader,
         children: [{ path: "/create-post", element: <NewPost /> }],
       },
     ],
